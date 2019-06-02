c2bf334b4d1129adf097ef6e3bd12aa2ba8c9ebc
JoshuaHolloway <jhollow6@asu.edu>
2019-06-02 04:11:23 -0500

    added swap function

--- Algorithms/Algorithms/src/helper.h
+++ Algorithms/Algorithms/src/helper.h
@@ -2,11 +2,27 @@
 #include <iostream>
 using std::cout;
 using std::endl;
-
+//---------------------------------------------------------
 void test(int* x, int len)
 {
 	for (int i = 0; i < len; ++i)
 		cout << x[i] << " ";
 	cout << endl;
 }
-
+//---------------------------------------------------------
+void swap(int& x, int& y)
+{
+	int temp = x;
+	x = y;
+	y = temp;
+}
+//---------------------------------------------------------
+void bubble_sort(int* x, int len)
+{
+	for (int i = 0; i < len - 1; ++i)
+	{
+		// compare current value and neighbor
+		if (x[i] > x[i + 1])
+			swap(x[i], x[i + 1]);
+	}
+}
--- Algorithms/Algorithms/src/main.cpp
+++ Algorithms/Algorithms/src/main.cpp
@@ -4,8 +4,11 @@ int main()
 {
 	int x[] = { 5, 4, 9, 1 };
 
-	cout << "Hello" << endl;
-	test(x, 4);
+	int a = 1;
+	int b = 2;
+	swap(a, b);
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
 	getchar();
 	return 0;
 }
